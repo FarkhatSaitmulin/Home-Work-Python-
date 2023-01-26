2b6558d3d012146ca35ab782b89720c2cbee4e3b
Frenk KAZ <farhats81@mail.ru>
2023-01-27 01:49:40 +0600

    решил первую задачу

--- seminar 1/HW seminar 1/task2.py
+++ seminar 1/HW seminar 1/task2.py
@@ -2,3 +2,9 @@
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0)
 
+number = int(input("Введите трехзначное число = "))
+result = number // 100 + number % 10 + (number//10) % 10
+print (result)
+
+
+1234577
