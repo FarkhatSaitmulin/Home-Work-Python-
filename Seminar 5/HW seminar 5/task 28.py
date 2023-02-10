aa7df54229be4e71c82fbae6ae6969e4075094be
Frenk KAZ <farhats81@mail.ru>
2023-02-10 23:05:41 +0600

    Исправил задачку 28 по 5 семинару

--- Seminar 5/HW seminar 5/task 28.py
+++ Seminar 5/HW seminar 5/task 28.py
@@ -5,9 +5,16 @@
 a = int(input('Введите число А ='))
 b = int(input('Введите число B ='))
 
-def sum(a,b):
-    if a and b < 0:
-        return (a+b)
-    return (a+b)
+# def sum(a,b):
+#     if a and b < 0:
+#         return (a+b)
+#     return (a+b)
+# print(a+b)
 
-print(sum(a,b))
+def sum(a, b):
+    if b == 0:
+        if a == 0:
+            return 0
+        return sum(a - 1, b) + 1
+    return sum(a, b - 1) + 1
+print(sum(a, b))
